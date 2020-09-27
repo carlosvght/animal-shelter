74f31662df0c3d157eb1c3829646756e62038014
Carlos <carlosvaghetti01@gmail.com>
2020-09-27 16:32:56 -0300

    added in animals_controller method create and created animals params

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,4 +6,20 @@ class AnimalsController < ApplicationController
   def new
     @animal = Animal.new
   end
+  
+  def create
+    @animal = Animal.new(animal_params)
+    
+    if @animal.save
+      redirect_to @animal
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :birth, :kind, :gender)
+  end
 end
