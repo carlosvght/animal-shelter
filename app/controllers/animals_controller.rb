93c8bac510b45d49d339f81eab2f668786f52007
Carlos <carlosvaghetti01@gmail.com>
2020-09-27 16:48:02 -0300

    added method update in controller, created view edit

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,13 +2,17 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
-
+  
   def show
     @animals = Animal.find(params[:id])
   end
-
+  
   def new
     @animal = Animal.new
+  end
+  
+  def edit
+    @animal = Animal.find(params[:id])
   end
   
   def create
@@ -20,9 +24,19 @@ class AnimalsController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def update
+    @animal = Animal.find(params[:id])
+    
+    if @animal.update(animal_params)
+      redirect_to @animal
+    else
+      render 'edit'
+    end
+  end
+  
   private
-
+  
   def animal_params
     params.require(:animal).permit(:name, :birth, :kind, :gender)
   end
